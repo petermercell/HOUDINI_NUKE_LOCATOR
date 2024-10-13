4d2b66b7ac5346d348533cc27290bd563d5b2e97
Peter Mercell <105944086+petermercell@users.noreply.github.com>
2024-10-13 21:10:23 +0200

    Add files via upload

---
+++ RECIPES_PATHs.py
@@ -0,0 +1,3 @@
+# RECIPE NAME: HOUDINI_NUKE_LOCATOR
+# WINDOWS_PATH: "C:\Users\WORKSTATION\Documents\houdini20.5\otls"
+# MAC_PATH: "/Applications/Houdini/Houdini20.5.370/Frameworks/Houdini.framework/Versions/20.5/Resources/houdini/otls"
